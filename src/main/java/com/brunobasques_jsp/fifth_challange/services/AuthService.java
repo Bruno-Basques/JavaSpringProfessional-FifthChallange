ae18d16c51d8a04f200520448b759e3881733bea
Bruno-Basques <bruno_basques@outlook.com>
2024-10-08 23:21:51 -0300

    Add Auth service

---
+++ src/main/java/com/brunobasques_jsp/fifth_challange/services/AuthService.java
@@ -0,0 +1,21 @@
+package com.brunobasques_jsp.fifth_challange.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunobasques_jsp.fifth_challange.entities.User;
+import com.brunobasques_jsp.fifth_challange.exceptions.ForbiddenException;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private UserService userService;
+	
+	public void validateSelfOrAdmin(long userId) {
+		User loggedUser = userService.authenticated();
+		if (!loggedUser.hasRole("ROLE_ADMIN") && !loggedUser.getId().equals(userId)) {
+			throw new ForbiddenException("Access denied");
+		}
+	}
+}
