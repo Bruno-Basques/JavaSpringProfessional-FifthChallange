72aeb215dc22a9de0ebc9fc54c0655d47941a5b4
Bruno-Basques <bruno_basques@outlook.com>
2024-10-08 22:38:04 -0300

    Add Category repository

---
+++ src/main/java/com/brunobasques_jsp/fifth_challange/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.brunobasques_jsp.fifth_challange.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brunobasques_jsp.fifth_challange.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
