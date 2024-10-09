be701f4ec5cb671302a8d2ab406f2f331b120436
Bruno-Basques <bruno_basques@outlook.com>
2024-10-08 21:51:22 -0300

    Add application properties and config files

---
+++ src/main/java/com/brunobasques_jsp/fifth_challange/JavaSpringProfessionalFifthChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.brunobasques_jsp.fifth_challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringProfessionalFifthChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringProfessionalFifthChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunobasques_jsp/fifth_challange/JavaSpringProfessionalFifthChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobasques_jsp.fifth_challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringProfessionalFifthChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
