3b3f1f9589b719a0d816d4d31a05d9cece9b2666
Bruno-Basques <bruno_basques@outlook.com>
2024-10-10 00:23:14 -0300

    Add Category controller and DTO;  security and mapper dependencies

---
+++ src/main/java/com/brunobasques_jsp/fifth_challange/controllers/CategoryController.java
@@ -0,0 +1,35 @@
+package com.brunobasques_jsp.fifth_challange.controllers;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brunobasques_jsp.fifth_challange.dtos.CategoryDTO;
+import com.brunobasques_jsp.fifth_challange.entities.Category;
+import com.brunobasques_jsp.fifth_challange.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<Category> entityList = service.findAll();
+        List<CategoryDTO> dtoList = entityList
+        		.stream()
+        		.map(x -> modelMapper.map(x, CategoryDTO.class))
+        		.toList();
+        return ResponseEntity.ok(dtoList);
+    }
+}
